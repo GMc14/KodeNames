ac80d9c7cc2e72e3f01ed18033a1a9b78b30ab15
GMc14 <git@gmc14.com>
2017-11-12 22:59:11 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -38,6 +38,9 @@ function fire() {
 		case 'movies':
 			sessionData = movieData.slice(0);
 			break;
+		case 'xmas':
+			sessionData = xmas.slice(0);
+			break;	
 		case 'custom':
 			if(customData.length === 0){
 				var customWordList = prompt("Please enter custom word list. The list will be saved until your refresh your browser. (The words MUST be delimanted by spaces). eg: cat dog mouse", "Enter words here");
